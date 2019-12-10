19d5e4acbaf0ef420b051a64debf2062336686f8
Percy <percival.ngan@gmail.com>
2019-12-10 16:52:53 -0600

    Added the load and saveToPersistentStore to StarController

--- Stars/Stars/Model Controller/StarController.swift
+++ Stars/Stars/Model Controller/StarController.swift
@@ -27,4 +27,32 @@ class StarController {
 		return dir.appendingPathComponent("stars.plist")
 	}
 
+	// Persistent has 2 methods: Read the data and save the data
+	private func loadFromPersistentStore() {
+		let fm = FileManager.default
+		guard let url = persistentFileURL,
+			fm.fileExists(atPath: url.path) else { return }
+
+		// The do try catch is a form of error handling
+		do {
+			let data = try Data(contentsOf: url) // anything that can throw an error you have to put try in front of it. In this case, Data can throw an error.
+			let decoder = PropertyListDecoder()
+			stars = try decoder.decode([Star].self, from: data)
+		} catch {
+			NSLog("Error loading stars data: \(error)")
+		}
+	}
+
+	private func saveToPersistentStore() {
+		guard let url = persistentFileURL else { return }
+
+		do {
+			let encoder = PropertyListEncoder()
+			let data = try encoder.encode(stars)
+			try data.write(to: url)
+		} catch {
+			NSLog("Error loading stars data: \(error)")
+		}
+	}
 }
+

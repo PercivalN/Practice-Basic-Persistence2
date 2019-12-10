b3ad071f64f7968b401ac16129a511fae16b575c
Percy <percival.ngan@gmail.com>
2019-12-10 17:03:12 -0600

    Added working method to printStar action button. Fully working app.

--- Stars/Stars/View Controllers/StarsViewController.swift
+++ Stars/Stars/View Controllers/StarsViewController.swift
@@ -9,31 +9,32 @@
 import UIKit
 
 class StarsViewController: UIViewController {
-
+	
 	@IBOutlet weak var nameTextField: UITextField!
 	@IBOutlet weak var distanceTextField: UITextField!
 	@IBOutlet weak var tableView: UITableView!
-
+	
 	// PROPERTIES:
 	let starController = StarController()
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
+		
 		tableView.dataSource = self // This creates a connection between the tableView data source and this view controller. This is only true is you have an extension that subclasses from a UITableViewDataSource.
 	}
-
+	
 	// MARK: Actions
 	@IBAction func printStars(_ sender: UIButton) {
+		print(starController.listStars())
 	}
-
+	
 	@IBAction func createStar(_ sender: UIButton) {
 		guard let name = nameTextField.text,
 			let distanceString = distanceTextField.text,
 			!name.isEmpty,
 			!distanceString.isEmpty,
 			let distance = Double(distanceString) else { return }
-
+		
 		// _ means that the value that is returned will not be used, the other option is to make the function @discardableResult in the model controller.
 		starController.createStar(named: name, withDistance: distance)
 		nameTextField.text = ""
@@ -47,13 +48,13 @@ extension StarsViewController: UITableViewDataSource {
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return starController.stars.count
 	}
-
+	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		guard let cell = tableView.dequeueReusableCell(withIdentifier: "StarCell", for: indexPath) as? StarTableViewCell else { return UITableViewCell() }
-
+		
 		let star = starController.stars[indexPath.row]
 		cell.star = star
-
+		
 		return cell
 	}
 }

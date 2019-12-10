fa63f6f124780e9449a52a8b7d867976cc0230e1
Percy <percival.ngan@gmail.com>
2019-12-09 18:58:03 -0600

    Refactored the ViewController to StarsViewController

---
+++ Stars/Stars/StarsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StarsViewController.swift
+//  Stars
+//
+//  Created by Percy Ngan on 12/9/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import UIKit
+
+class StarsViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+

e7fbec742576780bc49f7dff5e8ded88669ec02f
Percy <percival.ngan@gmail.com>
2019-12-09 21:35:52 -0600

    Added StarTableViewCell and subclass StarCell to it

---
+++ Stars/Stars/StarTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StarTableViewCell.swift
+//  Stars
+//
+//  Created by Percy Ngan on 12/9/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import UIKit
+
+class StarTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

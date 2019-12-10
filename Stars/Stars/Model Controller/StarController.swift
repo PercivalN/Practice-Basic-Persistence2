a349fa79cb292a3e3c8abd51baca60e64c2eac93
Percy <percival.ngan@gmail.com>
2019-12-10 14:14:04 -0600

    Added StarController model

---
+++ Stars/Stars/Model Controller/StarController.swift
@@ -0,0 +1,21 @@
+//
+//  StarController.swift
+//  Stars
+//
+//  Created by Percy Ngan on 12/9/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import Foundation
+
+// Storing data on dish should be taken up by a model controller
+class StarController {
+
+	private(set) var stars: [Star] = []
+
+	func createStar(named name: String, withDistance distance: Double) -> Star {
+		let star = Star(name: name, distance: distance)
+		stars.append(star)
+		return star
+	}
+}
---
+++ Stars/Stars/Models/Star.swift
@@ -0,0 +1,15 @@
+//
+//  Star.swift
+//  Stars
+//
+//  Created by Percy Ngan on 12/9/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import Foundation
+
+struct Star {
+	let name: String
+	let distance: Double
+
+}
